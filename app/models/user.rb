6673be902de767af1a56e87ad75c634de5fc9c5e
denpa7526 <d19.sax8kww@gmail.com>
2024-10-16 22:09:33 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { customer: 0, employee: 1 }
-  
+
+  with_options presence: true do
+    validates :username, uniqueness: true
+    validates :position
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10～11桁の半角数字でご入力ください' }
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字でご入力ください' } do
+      validates :last_name
+      validates :first_name
+    end
+  end
+
+  PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/
+  validates :password, format: { with: PASSWORD_REGEX, message: 'は半角英数字混合かつ6字以上でご入力ください' }, if: -> { new_record? || !password.nil? }
 
   def full_name
     "#{last_name} #{first_name}"
