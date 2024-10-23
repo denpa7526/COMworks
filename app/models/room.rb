bc0d527f814c353fee83781ae9b442ac505a88cd
denpa7526 <d19.sax8kww@gmail.com>
2024-10-23 15:31:01 +0900

    プルダウンの基礎実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ApplicationRecord
-  belongs_to :created_by, class_name: 'User'
+  belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
+
+  enum room_type: { private_room: 0, public_room: 1 }
+  
 end
